411eb1de2708ef36c80437bb1c20f8ddaf5ca57b
VICTOR MANUEL ITURBIDE MARIANO <iturbide.mariano.victor.manuel@gmail.com>
2026-03-02 02:12:24 -0600

    NOVENO ARCHIVO CLASES ASBTRACTAS (SEGUNDA PARTE COMPLETA) EN DART

--- (09)_CLASES_ABSTRACTAS.dart
+++ (09)_CLASES_ABSTRACTAS.dart
@@ -1,12 +1,26 @@
 void main() {
   final windPlant = WindPlant(initialEnergy: 100, /type: "Wind"/); //Ahora se instancia la WindPlant 
+  final nuclearPlant = NuclearPlant(energyLeft: 1000);
   
-  
-  print('Planta: $windPlant');
+  print('Wind: ${chargePhone(windPlant)}');
+  print('Nuclear: ${chargePhone(nuclearPlant)}');
+  //print('Planta: $windPlant');
 }
+
+
+double chargePhone(EnergyPlant plant){
+  if(plant.energyLeft <10){
+    throw Exception('Not enought energy');
+  }
+  return plant.energyLeft -10;
+} 
+  
+enum PlantType{nuclear, wind, water}
+
+
  abstract class EnergyPlant {
    double energyLeft;
-   final String type;
+   final PlantType type;
    
    //Constructor
    EnergyPlant({
@@ -15,14 +29,19 @@ void main() {
      });
    
    //Creamos un metodo sobre el consumo de energia
-   void consumeEnergy(double amount){
+   void consumeEnergy(double amount)
+   /*{
      energyLeft -= amount;
-   }
+   }*/ // Se quito porque cada planta tiene un consumo diferente, por lo tanto se implementa en cada clase hija
  }
 
 class WindPlant extends EnergyPlant{
   WindPlant({required double initialEnergy})
-    : super(energyLeft: initialEnergy, type : "Wind"); //Para usar los atributos de la clase abstracta mandamos a llamar a dicha clase con la palabra reservada super
+    : super(energyLeft: initialEnergy, type : PlantType.wind /*"Wind"*/); //Para usar los atributos de la clase abstracta mandamos a llamar a dicha clase con la palabra reservada super
+    @override
+    void consumeEnergy(double amount){
+    energyLeft -= amount; 
+  }
 }
 
 class NuclearPlant extends EnergyPlant{
@@ -31,11 +50,14 @@ class NuclearPlant extends EnergyPlant{
   double energyLeft;
   
   @override
-  final String type = "nuclear";
-  
-  NuclearPlant({required double energyLeft});
+  //final String type ="nuclear";
+
+  NuclearPlant({required /*double*/ this.energyLeft});
+  @override
+  void consumeEnergy(double amount){
+    energyLeft -= (amount * 0.5); 
+  }
 }
     
 //Clases abstractas, son un tipo de meta clase, lo cual nos permiten crear otras clases pero no es herencia
 //Añadimos la palabra abstrac antes dek nombre de la clase para volverlo abstracta
-//planta nuclear (pendiente de acompletar)
